416071ee7c8a868ce547bdd55b4eaa44c443a5e6
BelieveGamesStudios <afolabifunmilade229@gmail.com>
2025-08-07 11:40:48 +0100

    Task 1, 3 and 4 commit

--- Assets/Scripts/AI/LabAssistant.cs
+++ Assets/Scripts/AI/LabAssistant.cs
@@ -7,21 +7,43 @@ namespace TeamSix
     public class LabAssistant : MonoBehaviour
     {
         [Header("Component")]
+        private Animator animator;
         private AudioSource dialogueSource;
         private NavMeshAgent agent;
 
 
+        [Header("Player Tracking")]
+        [Range(0.01f, 4)]
+        [SerializeField] private float followDistance = 3f; // Distance at which the assistant will follow the player
+        private Transform player;
+
 
         [Header("Dialogue")]
         [SerializeField] private Dialogue[] dialogues;
+        [Range(0, 15)]
+        [SerializeField] private float dialogueDelay;
+        private float nextTimeToTalk;
+
+        [Header("Animator Hashes")]
+        private int motion = Animator.StringToHash("Motion");
+        private int talk = Animator.StringToHash("Talk");
         private void OnEnable()
         {
-            agent=GetComponent<NavMeshAgent>();
+            animator = GetComponent<Animator>();
+            agent = GetComponent<NavMeshAgent>();
             dialogueSource = GetComponent<AudioSource>();
+            player = Camera.main.transform;
             PlayDialogue("Welcome Message");
+        }
+        private void Update()
+        {
+            FollowPlayer();
+            if (!dialogueSource.isPlaying)
+                animator.SetBool(talk, false);
         }
         public void PlayDialogue(string dialogueTag)
         {
+            if (Time.time < nextTimeToTalk) return;
             foreach (var dialogue in dialogues)
             {
                 if (dialogue.Title == dialogueTag)
@@ -30,15 +52,55 @@ namespace TeamSix
                     {
                         dialogueSource.clip = dialogue.AudioClip;
                         dialogueSource.Play();
-                    }
-                    else
-                    {
-                        Debug.LogWarning("AudioSource or AudioClip is missing for dialogue: " + dialogueTag);
+                        animator.SetBool(talk, true);
+                        nextTimeToTalk = Time.time + dialogueDelay;
+
                     }
                     return;
                 }
             }
             Debug.LogWarning("Dialogue with tag '" + dialogueTag + "' not found.");
+            nextTimeToTalk = Time.time + dialogueDelay;
+        }
+        public void ShutUp()
+        {
+            if (dialogueSource != null && dialogueSource.isPlaying)
+            {
+                dialogueSource.Stop();
+            }
+            animator.SetBool(talk, false);
+        }
+        void FollowPlayer()
+        {
+
+            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+            if (distanceToPlayer >= followDistance)
+            {
+                animator.SetFloat(motion, Mathf.Lerp(animator.GetFloat(motion), 1, 10 * Time.deltaTime));
+                agent.SetDestination(player.position);
+                agent.isStopped = false;
+            }
+            else
+            {
+                animator.SetFloat(motion, Mathf.Lerp(animator.GetFloat(motion), 0, 10 * Time.deltaTime));
+                LookAtPlayer();
+                agent.isStopped = true;
+            }
+        }
+        void LookAtPlayer()
+        {
+            Vector3 toPlayer = Camera.main.transform.position - transform.position;
+            toPlayer = toPlayer.normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(toPlayer.x, 0, toPlayer.z));
+            transform.rotation = lookRotation;
+        }
+        private void OnAnimatorMove()
+        {
+            float speed = 0;
+            speed = (animator.deltaPosition / Time.deltaTime).magnitude;
+            if (float.IsNaN(speed) == true)
+                speed = 0;
+            agent.speed = speed;
         }
     }
     [System.Serializable]
--- Assets/Scripts/Custom Interactables/Fridge.cs
+++ Assets/Scripts/Custom Interactables/Fridge.cs
@@ -1,4 +1,5 @@
 using Believe.Games.Studios;
+using UnityEngine.UI;
 using UnityEngine;
 
 namespace TeamSix
@@ -7,17 +8,19 @@ namespace TeamSix
     {
         [Header("Components")]
         [SerializeField] private Animator fridgeAnimator;
-
+        [SerializeField] private Text fridgeText;
 
         private bool fridgeDoorOpen = false;
         private int doorHash=Animator.StringToHash("Door");
         public override void Interact()
         {
             fridgeDoorOpen=!fridgeDoorOpen;
-
+            
             fridgeAnimator.SetBool(doorHash,fridgeDoorOpen);
             string fridgeSound = fridgeDoorOpen ? "Fridge Open" : "Fridge Close";
             AudioManager.Instance.PlaySound(fridgeSound);
+            string fridgeButtonText = fridgeDoorOpen ? "Close Fridge" : "Open Fridge";
+            fridgeText.text = fridgeButtonText;
         }
     }
 }
